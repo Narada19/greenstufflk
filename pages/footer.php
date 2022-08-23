34828389e9de21e67d2f13b2de07860d48bbee8b
narada19 <tnarade@gmail.com>
2022-08-23 22:27:38 +0530

    Fixing issue in landing pages

---
+++ pages/footer.php
@@ -0,0 +1,121 @@
+            <!-- Footer Area Start Here -->
+            <footer>
+                <div class="footer-area-top">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                <div class="footer-box">
+                                    <h3>About Us</h3>
+                                    <p>GREEN STUFF PVT LTD Is a leader plant exporting and landscaping company in Sri Lanka… We are Fitting and 
+                                        Landscaping your dream garden. we Will bring the design free for you. Our company is a chained group of 
+                                        companies with Dubai and Sri Lanka! We have the largest plant collection Plant and many More Garden Services and accessories.</p>
+                                    <ul class="footer-social">
+                                        <li><a href="https://www.facebook.com/greenstuffpvtltd/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                        <li><a href="https://twitter.com/GreenStufflk" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                        <li><a href="" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                        <li><a href="https://www.instagram.com/greenstuffpvtltd/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                        <li><a href="https://www.pinterest.com/greenstufflk" target="_blank"><i class="fa fa-pinterest"  aria-hidden="true"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                <div class="footer-box">
+                                    <h3>Facebook Page</h3>
+                                    <div class="fb-page" data-href="https://www.facebook.com/greenstuffpvtltd/" data-tabs="timeline" data-width="" data-height="300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                                        <blockquote cite="https://www.facebook.com/greenstuffpvtltd/" class="fb-xfbml-parse-ignore">
+                                            <a href="https://www.facebook.com/greenstuffpvtltd/">GREEN STUFF PVT LTD</a>
+                                        </blockquote>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                <div class="footer-box">
+                                    <h3>Gallery</h3>
+                                    <ul class="flickr-photos">
+                                        <li><a href="#"><img class="img-responsive" src="img/footer/flickr-1.jpg" alt="flickr1"></a></li>
+                                        <li><a href="#"><img class="img-responsive" src="img/footer/flickr-2.jpg" alt="flickr2"></a></li>
+                                        <li><a href="#"><img class="img-responsive" src="img/footer/flickr-3.jpg" alt="flickr3"></a></li>
+                                        <li><a href="#"><img class="img-responsive" src="img/footer/flickr-4.jpg" alt="flickr4"></a></li>
+                                        <li><a href="#"><img class="img-responsive" src="img/footer/flickr-5.jpg" alt="flickr5"></a></li>
+                                        <li><a href="#"><img class="img-responsive" src="img/footer/flickr-6.jpg" alt="flickr6"></a></li>
+                                        <li><a href="#"><img class="img-responsive" src="img/footer/flickr-7.jpg" alt="flickr4"></a></li>
+                                        <li><a href="#"><img class="img-responsive" src="img/footer/flickr-8.jpg" alt="flickr5"></a></li>
+                                        <li><a href="#"><img class="img-responsive" src="img/footer/flickr-9.jpg" alt="flickr6"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                <div class="footer-box">
+                                    <h3>Contact US</h3>
+                                    <ul class="corporate-address">
+                                        <li><i class="fa fa-phone" aria-hidden="true"></i>(+94) 11 211 9733</li>
+                                        <li><i class="fa fa-map-marker" aria-hidden="true"></i><a href="#" class="text-white"> 214/a Sudarshana Mawatha, Malabe 10115 </a></li>
+                                    </ul>
+                                    <div class="newsletter-area">
+                                        <h3>Subcribe</h3>
+                                        <div class="input-group stylish-input-group">
+                                            <input type="text" placeholder="Enter your e-mail here" class="form-control">
+                                            <span class="input-group-addon">
+                                            <button type="submit">
+                                            <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                                            </button>  
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-area-bottom">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                                <p>&copy; 2022 greenstufflk All Rights Reserved.</p>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                                <ul class="payment-method">
+                                    <li><a href="#"><img alt="payment-method" src="img/payment-method1.jpg"></a></li>
+                                    <li><a href="#"><img alt="payment-method" src="img/payment-method2.jpg"></a></li>
+                                    <li><a href="#"><img alt="payment-method" src="img/payment-method3.jpg"></a></li>
+                                    <li><a href="#"><img alt="payment-method" src="img/payment-method4.jpg"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- Footer Area End Here -->
+        </div>
+        <!-- Preloader Start Here -->
+        <div id="preloader"></div>
+        <!-- Preloader End Here -->
+        <!-- jquery-->	
+        <script data-cfasync="false" src="../../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+        <script src="js/vendor/jquery-2.2.4.min.js"></script>
+        <!-- bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Owl Cauosel JS -->
+        <script src="js/owl.carousel.min.js"></script>
+        <!-- Nivo slider js -->		
+        <script src="lib/custom-slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+        <script src="lib/custom-slider/home.js" type="text/javascript"></script>
+        <!-- Meanmenu Js -->
+        <script src="js/jquery.meanmenu.min.js"></script>
+        <!-- WOW JS -->		
+        <script src="js/wow.min.js"></script>
+        <!-- plugins js -->
+        <script src="js/plugins.js"></script>
+        <!-- jQuery Srollup -->
+        <script src="js/jquery.scrollUp.min.js" type="text/javascript"></script>
+        <!-- jQuery Isotope -->
+        <script src="js/isotope.pkgd.min.js" type="text/javascript"></script>
+        <!-- jquery.counterup js -->
+        <script src="js/jquery.counterup.min.js"></script>
+        <script src="js/waypoints.min.js"></script>
+        <!-- Validator js -->
+        <script src="js/validator.min.js" type="text/javascript"></script>
+        <!-- Custom Js -->
+        <script src="js/main.js"></script>  
+    </body>
+</html>
---
+++ pages/header.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title> Green Stuff | Home </title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Animate CSS --> 
+        <link rel="stylesheet" href="css/animate.min.css">
+        <!-- font-awesome CSS-->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <!-- Owl Caousel CSS -->
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/owl.theme.default.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+        <!-- main menu CSS-->		
+        <link rel="stylesheet" href="css/meanmenu.min.css">
+        <!-- nivo slider CSS -->
+        <link rel="stylesheet" href="lib/custom-slider/css/nivo-slider.css" type="text/css" />
+        <link rel="stylesheet" href="lib/custom-slider/css/preview.css" type="text/css" media="screen" />
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="style.css">
+        <!-- Modernizr Js -->
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script async defer src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v14.0&appId=370252033038305&autoLogAppEvents=1" nonce="R6cN2Wkm"></script>
+    </head>
+    <body>
+        <div id="fb-root"></div>
+        <div class="wrapper-area">
+            <!-- Header Area Start Here -->
+            <header>
+                <div class="header-style1-area">
+                    <div class="header-top-area">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                                    <div class="header-top-left">
+                                        <ul class="header-social">
+                                            <li><a href="https://www.facebook.com/greenstuffpvtltd/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                            <li><a href="https://twitter.com/GreenStufflk" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                            <li><a href="https://www.linkedin.com/company/greenstufflk/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                            <li><a href="https://www.instagram.com/greenstuffpvtltd/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                            <li><a href="https://www.pinterest.com/greenstufflk" target="_blank"><i class="fa fa-pinterest"  aria-hidden="true"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+                                    <div class="header-top-right">
+                                        <ul>
+                                            <li><i class="fa fa-map-marker" aria-hidden="true"></i><a href="#"> 214/a Sudarshana Mawatha, Malabe 10115 </a></li>
+                                            <li><i class="fa fa-phone" aria-hidden="true"></i><a href="Phone:+94112119733"> (+94) 11 211 9733 </a></li>
+                                            <li></li><button class="btn-submit" type="submit">Contact Us </button></ul>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="header-bottom-area" id="sticker">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                                    <div class="logo-area">
+                                        <div class="logo-area">
+                                            <a href="index.html"><img class="img-responsive" src="img/logo.png" alt="logo"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
+                                    <div class="main-menu-area">
+                                        <nav>
+                                            <ul>
+                                                <li><a href="index.html">Home</a></li>
+                                                <li><a href="about.html">About Us</a></li>
+                                                <li><a href="services.html">Services</a></li>
+                                                <li><a href="shop.html">Shop</a></li>
+                                                <li><a href="news.html">News</a></li>
+                                                <li><a href="contact.html">Contact</a></li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Mobile Menu Area Start -->
+                <div class="mobile-menu-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="mobile-menu">
+                                    <nav id="dropdown">
+                                        <ul>
+                                            <li><a href="index.html">Home</a></li>
+                                            <li><a href="about.html">About Us</a></li>
+                                            <li><a href="services.html">Services</a></li>
+                                            <li><a href="shop.html">Shop</a></li>
+                                            <li><a href="news.html">News</a></li>
+                                            <li><a href="contact.html">Contact</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Mobile Menu Area End -->
+            </header>
+            <!-- Header Area End Here -->
